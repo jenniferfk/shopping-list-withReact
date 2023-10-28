f85c8f232b1b1c8b41ddc4e8640e2714a25af3aa
jenniferfk <jenny.kamel002@gmail.com>
2023-10-28 21:39:40 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import jsPDF from "jspdf";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [item,setItem]=useState("");
+  const [itemsList, setItemsList] = useState([]);
+
+  function onSubmit(event){
+    event.preventDefault();
+    if (item.trim() !== "") {
+      setItemsList([...itemsList, item]);
+      setItem(""); 
+    }
+
+  }
+  function removeItem(index) {
+    const updatedItems = [...itemsList];
+    updatedItems.splice(index, 1);
+    setItemsList(updatedItems);
+  }
+  function generatePDF() {
+    const doc = new jsPDF();
+    const content = itemsList.map((item, index) => `${index + 1}. ${item}`).join("\n");
+    doc.text(content, 10, 10);
+    doc.save("shopping-list.pdf");
+  }
+
+  
+ return(
+  <div>
+    <h1>Shopping List</h1>
+    <div className="pdfdownload"><button onClick={generatePDF} className="downloadbttn">Download List</button></div>
+    <div className="listcontainer">
+      <h2>List</h2>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="items"
+          placeholder="Add an item"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button type="submit">
+          ADD
+        </button>
+      </form>
+      <ul>
+          {itemsList.map((listItem, index) => (
+            <li key={index}>
+              {listItem}
+              <button className="removeitem" onClick={() => removeItem(index)}>x</button>
+            </li>
+          ))}
+        </ul>
     </div>
-  );
+  </div>
+ );
 }
 
 export default App;
